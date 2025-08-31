1b0d59602fbc565245f1f64b3f9df3b7d2546e11
Shaked Hagag <shaked@frame.security>
2025-08-31 11:40:13 +0300

    oil configs and opencode integration

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,15 +1,27 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    view_options = {
-      show_hidden = true,
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
     },
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
   },
-  -- Optional dependencies
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+  -- {
+  --   'benomahony/oil-git.nvim',
+  --   dependencies = { 'stevearc/oil.nvim' },
+  --   -- No opts or config needed! Works automatically
+  -- },
+  {
+    'JezerM/oil-lsp-diagnostics.nvim',
+    dependencies = { 'stevearc/oil.nvim' },
+    opts = {},
+  },
 }
