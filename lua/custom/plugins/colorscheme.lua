a9711deb648d67042ef6ce81a41303dea0f511e8
Shaked Hagag <shaked@frame.security>
2025-09-08 18:00:28 +0300

    favs colorschemes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,6 +1,25 @@
 return {
-  'craftzdog/solarized-osaka.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {},
+  {
+    'craftzdog/solarized-osaka.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   config = function()
+  --     vim.cmd 'colorscheme rose-pine'
+  --   end,
+  -- },
+  {
+    'tiagovla/tokyodark.nvim',
+    opts = {
+      -- custom options here
+    },
+    config = function(_, opts)
+      require('tokyodark').setup(opts) -- calling setup is optional
+      vim.cmd [[colorscheme tokyodark]]
+    end,
+  },
 }
